a8b0219e62c548e2fc5a583254e83479e67e6266
bhanuPrakashReddy2105 <bhanuprakashreddy_v@apollolifeline.com>
2021-05-20 15:20:46 +0530

    Todolist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:TodosComponent
+  },
+  {
+    path: 'todolist',
+    component:TodosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-todos',
@@ -9,7 +11,29 @@ export class TodosComponent implements OnInit {
 
   constructor() { }
 
+  title: any
+
+  todos: string[] = ['Bhanu', 'prakash']
+
+  todo = new FormGroup({
+    check: new FormControl(false),
+    item: new FormControl("",[Validators.required])
+  })
+
   ngOnInit(): void {
+    this.title = moment().format('dddd');
+  }
+
+  addTodo() {
+    let todo = this.todo.get('item')?.value
+    this.todos.push(todo);
+    this.todo.get('item')?.setValue("");
+  }
+
+  onCheckboxChange(event: any, todo: string) {
+    if (event.checked) {
+      this.todos = this.todos.filter((x) => x != todo)
+    }
   }
 
 }
